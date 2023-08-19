7fe667613e8c02d6af9024ec8d898868737e634c
sami5671 <samialam5671@gmail.com>
2023-08-19 13:58:22 +0600

    update

--- js/login.js
+++ js/login.js
@@ -15,7 +15,7 @@ document.getElementById("btn-submit").addEventListener("click", function () {
   //   Danger: do not varify eamil password on the client side
   // step: 4: varify eamil and password and check weather valid user or not
   if (email === "samialam@gmail.com" && password === "sami123") {
-    window.location.href = "login.html";
+    window.location.href = "bank.html";
   } else {
     alert("!!!!*****invalid user!!!!!*******");
   }
